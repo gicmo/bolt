73bd20d4b3f05654c94758e6c5796e3bb275a56e
Christian Kellner <christian@kellner.me>
2017-07-24 13:21:16 +0200

    db: Implement get, set

--- db.c
+++ db.c
@@ -51,6 +51,170 @@ print_device (TbDevice *dev)
   g_print ("\n");
 }
 
+static int
+list (TbStore *store)
+{
+  g_autoptr(GError) error = NULL;
+  g_auto(GStrv) ids       = NULL;
+  char **id;
+
+  ids = tb_store_list_ids (store, &error);
+  if (ids == NULL)
+    return EXIT_FAILURE;
+
+  for (id = ids; *id; id++)
+    {
+      g_autoptr(TbDevice) dev = tb_store_get (store, *id, &error);
+
+      if (dev == NULL)
+        continue;
+
+      print_device (dev);
+    }
+
+  return EXIT_SUCCESS;
+}
+
+static char *known_fields[] = {"auto"};
+
+static int
+get (TbStore *store, int argc, char **argv)
+{
+  g_autoptr(GError) error = NULL;
+  g_autoptr(TbDevice) dev = NULL;
+  const char *uid;
+  char **fields;
+  int n, i;
+
+  if (argc < 3)
+    return EXIT_FAILURE;
+
+  uid = argv[2];
+
+  dev = tb_store_get (store, uid, &error);
+
+  if (!dev)
+    {
+      g_printerr ("Could not get device: %s", error->message);
+      return EXIT_FAILURE;
+    }
+
+  if (argc > 3)
+    {
+      fields = argv + 3;
+      n      = argc - 3;
+    }
+  else
+    {
+      fields = known_fields;
+      n      = G_N_ELEMENTS (known_fields);
+    }
+
+  for (i = 0; i < n; i++)
+    {
+      const char *field = fields[i];
+      if (g_str_equal (field, "auto"))
+        {
+          gboolean do_auto = tb_device_autoconnect (dev);
+          g_print ("auto: %s\n", do_auto ? "yes" : "no");
+        }
+      else
+        {
+          g_warning ("Unknown field: %s", field);
+        }
+    }
+
+  return EXIT_SUCCESS;
+}
+
+static gboolean
+string_to_bool (const char *str, gboolean *b)
+{
+  if (!g_ascii_strcasecmp (str, "yes"))
+    {
+      *b = TRUE;
+      return TRUE;
+    }
+  else if (!g_ascii_strcasecmp (str, "no"))
+    {
+      *b = FALSE;
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+static int
+set (TbStore *store, int argc, char **argv)
+{
+  g_autoptr(GError) error = NULL;
+  g_autoptr(TbDevice) dev = NULL;
+  gboolean ok;
+  const char *uid;
+  char **fields;
+  int n, i;
+
+  if (argc < 3)
+    return EXIT_FAILURE;
+
+  uid = argv[2];
+
+  dev = tb_store_get (store, uid, &error);
+
+  if (!dev)
+    {
+      g_printerr ("Could not get device: %s", error->message);
+      return EXIT_FAILURE;
+    }
+
+  if (argc > 3)
+    {
+      fields = argv + 3;
+      n      = argc - 3;
+    }
+  else
+    {
+      fields = known_fields;
+      n      = G_N_ELEMENTS (known_fields);
+    }
+
+  for (i = 0; i < n; i++)
+    {
+      const char *field   = fields[i];
+      g_auto(GStrv) split = g_strsplit (field, "=", 2);
+      const char *key, *value;
+      if (g_strv_length (split) != 2)
+        {
+          g_printerr ("wrong argument: %s\n", field);
+          return EXIT_FAILURE;
+        }
+
+      key   = split[0];
+      value = split[1];
+
+      if (g_str_equal (key, "auto"))
+        {
+          gboolean b;
+
+          ok = string_to_bool (value, &b);
+          if (!ok)
+            {
+              g_fprintf (stderr, "Could not convert '%s' to boolean\n", value);
+              return EXIT_FAILURE;
+            }
+
+          dev->autoconnect = b;
+        }
+    }
+
+  ok = tb_store_put (store, dev, &error);
+
+  if (!ok)
+    g_printerr ("Could not store device changes: %s\n", error->message);
+
+  return EXIT_SUCCESS;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -92,29 +256,13 @@ main (int argc, char **argv)
   store = g_object_new (TB_TYPE_STORE, "root", root, NULL);
 
   if (g_str_equal (cmd, "list"))
-    {
-      g_auto(GStrv) ids = tb_store_list_ids (store, &error);
-      char **id;
-
-      if (ids == NULL)
-        return EXIT_FAILURE;
-
-      for (id = ids; *id; id++)
-        {
-          g_autoptr(TbDevice) dev = tb_store_get (store, *id, &error);
-
-          if (dev == NULL)
-            continue;
-
-          print_device (dev);
-        }
-
-      res = EXIT_SUCCESS;
-    }
+    res = list (store);
+  else if (g_str_equal (cmd, "get"))
+    res = get (store, argc, argv);
+  else if (g_str_equal (cmd, "set"))
+    res = set (store, argc, argv);
   else
-    {
-      g_printerr ("Unknown command.\n");
-    }
+    g_printerr ("Unknown command.\n");
 
   return res;
 }
