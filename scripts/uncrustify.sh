0b46fd27b6a48baf0948215c679e73833e69e768
Christian Kellner <christian@kellner.me>
2018-01-18 15:29:18 +0100

    scripts: fix uncrustify.sh: proper cfg location
    It was looking in the old location (contrib) not in scripts. Doh.

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SRCROOT=`git rev-parse --show-toplevel`
-CFG="$SRCROOT/contrib/uncrustify.cfg"
+CFG="$SRCROOT/scripts/uncrustify.cfg"
 echo "srcroot: $SRCROOT"
 pushd "$SRCROOT"
 uncrustify -c "$CFG" --no-backup `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v gvdb | grep -v build/`
