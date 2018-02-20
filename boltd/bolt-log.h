fbcfd2e2ab7f11b95c685acfff09b62578bef3b8
Christian Kellner <christian@kellner.me>
2018-02-20 16:55:27 +0100

    logging: custom (structured) logging functions
    This change consists of two parts:
    a) New logging functions/macros that understand special keywords,
       like "@device", "@topic", "@error"; these keywords will be
       converted to key value pairs. For device and error the
       corresponding objects will also be stored in a special log
       context (BoltLogCtx).
    b) Two log writer functions to log to the journal and standard
       streams. These writer functions will also make use of the
       BoltLogCtx during formatting.
    
    Both parts should be used together but can also work independently.

---
+++ boltd/bolt-log.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright © 2018 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Christian J. Kellner <christian@kellner.me>
+ */
+
+#pragma once
+
+#include "bolt-names.h"
+
+#include <gio/gio.h>
+
+#include <stdarg.h>
+
+
+G_BEGIN_DECLS
+
+#define LOG_SPECIAL_CHAR '@'
+#define LOG_PASSTHROUGH_CHAR '_'
+
+#define LOG_DIRECT(k, v) "_" k, v
+#define LOG_DEV(device) "@device", device
+#define LOG_ERR(error) "@error", error
+#define LOG_TOPIC(topic) "@topic", topic
+#define LOG_DEV_UID(uid) LOG_DIRECT (BOLT_LOG_DEVICE_UID, uid)
+
+#define bolt_debug(...) bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG,                 \
+                                  LOG_DIRECT ("CODE_FILE", __FILE__),               \
+                                  LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)), \
+                                  LOG_DIRECT ("CODE_FUNC", G_STRFUNC),              \
+                                  __VA_ARGS__)
+
+#define bolt_info(...) bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_INFO,                   \
+                                 LOG_DIRECT ("CODE_FILE", __FILE__),               \
+                                 LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)), \
+                                 LOG_DIRECT ("CODE_FUNC", G_STRFUNC),              \
+                                 __VA_ARGS__)
+
+#define bolt_msg(...) bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_MESSAGE,                \
+                                LOG_DIRECT ("CODE_FILE", __FILE__),                \
+                                LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)),  \
+                                LOG_DIRECT ("CODE_FUNC", G_STRFUNC),               \
+                                __VA_ARGS__)
+
+#define bolt_warn(...) bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_WARNING,                 \
+                                 LOG_DIRECT ("CODE_FILE", __FILE__),                \
+                                 LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)),  \
+                                 LOG_DIRECT ("CODE_FUNC", G_STRFUNC),               \
+                                 __VA_ARGS__)
+
+#define bolt_critical(...) bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_CRITICAL,                \
+                                     LOG_DIRECT ("CODE_FILE", __FILE__),                \
+                                     LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)),  \
+                                     LOG_DIRECT ("CODE_FUNC", G_STRFUNC),               \
+                                     __VA_ARGS__)
+
+#define bolt_error(...)  G_STMT_START {                                  \
+    bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_ERROR,                          \
+              LOG_DIRECT ("CODE_FILE", __FILE__),                       \
+              LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)),         \
+              LOG_DIRECT ("CODE_FUNC", G_STRFUNC),                      \
+              __VA_ARGS__);                                             \
+    for (;; ) { }                                                       \
+} G_STMT_END
+
+#define bolt_warn_err(e, ...) bolt_log (G_LOG_DOMAIN, G_LOG_LEVEL_WARNING,                 \
+                                        LOG_DIRECT ("CODE_FILE", __FILE__),                \
+                                        LOG_DIRECT ("CODE_LINE", G_STRINGIFY (__LINE__)),  \
+                                        LOG_DIRECT ("CODE_FUNC", G_STRFUNC),               \
+                                        LOG_ERR (e),                                       \
+                                        __VA_ARGS__)
+
+void               bolt_logv (const char    *domain,
+                              GLogLevelFlags level,
+                              va_list        args);
+
+void               bolt_log (const char    *domain,
+                             GLogLevelFlags level,
+                             ...);
+
+
+/* consumer functions */
+
+typedef struct _BoltLogCtx BoltLogCtx;
+
+BoltLogCtx *       bolt_log_ctx_acquire (const GLogField *fields,
+                                         gsize            n);
+
+void               bolt_log_ctx_free (BoltLogCtx *ctx);
+
+const char *       blot_log_ctx_get_domain (BoltLogCtx *ctx);
+
+GLogWriterOutput   bolt_log_stdstream (const BoltLogCtx *ctx,
+                                       GLogLevelFlags    log_level,
+                                       guint             flags);
+
+GLogWriterOutput    bolt_log_journal (const BoltLogCtx *ctx,
+                                      GLogLevelFlags    log_level,
+                                      guint             flags);
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (BoltLogCtx, bolt_log_ctx_free);
+
+G_END_DECLS
