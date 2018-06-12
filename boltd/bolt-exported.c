aa40de44a2a04a922a8d74aba8185c4c08ce8206
Christian Kellner <christian@kellner.me>
2018-06-12 17:12:44 +0200

    exported: helper to manually override properties
    The normal way to override existing properties for a gobject is via
    g_object_class_override_property, which internally generates a
    GParamSpecOverride and call g_object_class_install_property. In bolt
    we normally use g_object_class_install_properties which expects an
    array of GParamSpecs. Same for bolt_exported_class_export_properties.
    Therefore we craft a helper function that looks up the property and
    creates the GParamSpecOverride but does not yet install it, so we
    can then install it together with all the other normal properties.

--- boltd/bolt-exported.c
+++ boltd/bolt-exported.c
@@ -1317,3 +1317,46 @@ bolt_exported_prop_gvariant_to_gvalue (BoltExportedProp *prop,
 
   return TRUE;
 }
+
+GParamSpec *
+bolt_param_spec_override (GObjectClass *klass,
+                          const char   *name)
+{
+  GObjectClass *parent_class = NULL;
+  GParamSpec *base = NULL;
+  GType parent;
+  GType type;
+  guint n;
+
+  g_return_val_if_fail (G_IS_OBJECT_CLASS (klass), NULL);
+  g_return_val_if_fail (name != NULL, NULL);
+
+  type = G_OBJECT_CLASS_TYPE (klass);
+  parent = g_type_parent (type);
+
+  parent_class = g_type_class_peek (parent);
+  if (parent_class)
+    base = g_object_class_find_property (parent_class, name);
+
+  if (base == NULL)
+    {
+      g_autofree GType *ifaces = g_type_interfaces (type, &n);
+
+      while (n-- && base == NULL)
+        {
+          gpointer iface = g_type_default_interface_peek (ifaces[n]);
+
+          if (iface)
+            base = g_object_interface_find_property (iface, name);
+        }
+    }
+
+  if (base == NULL)
+    {
+      bolt_bug ("Could not override unknown property: '%s::%s'",
+                G_OBJECT_CLASS_NAME (klass), name);
+      return NULL;
+    }
+
+  return g_param_spec_override (name, base);
+}
