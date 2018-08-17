a090a6826ecab1894c725e07346ab1fd840fe221
Christian Kellner <christian@kellner.me>
2018-08-17 14:16:31 +0200

    test: power: check recovering of non-boltd guards
    Power guards can be internal (created for boltd) or external, i.e.
    created for an external process (in the future via a D-Bus API).
    Check that we successfully recover non-internal power guards.

--- tests/test-power.c
+++ tests/test-power.c
@@ -22,6 +22,7 @@
 
 #include "bolt-power.h"
 
+#include "bolt-fs.h"
 #include "bolt-str.h"
 #include "mock-sysfs.h"
 
@@ -35,6 +36,8 @@
 #include <libudev.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 typedef struct udev_device udev_device;
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (udev_device, udev_device_unref);
@@ -428,6 +431,116 @@ test_power_recover_state (TestPower *tt, gconstpointer user)
   g_unsetenv ("BOLT_RUNDIR");
 }
 
+
+static void
+test_power_recover_guards_ok (TestPower *tt, gconstpointer user)
+{
+  g_autoptr(BoltPower) power = NULL;
+  g_autoptr(BoltPowerGuard) guard = NULL;
+  g_autoptr(GError) err = NULL;
+  BoltPowerState state;
+  const char *fp;
+  pid_t pid;
+  int r;
+
+  fp = mock_sysfs_force_power_add (tt->sysfs);
+  g_assert_nonnull (fp);
+
+  pid = fork ();
+  g_assert_cmpint (pid, !=, -1);
+
+  if (pid == 0)
+    {
+      /* child */
+      power = make_bolt_power_timeout (tt, 10);
+
+      g_assert_no_error (err);
+      g_assert_nonnull (power);
+
+      state = bolt_power_get_state (power);
+      g_assert_cmpint (state, ==, BOLT_FORCE_POWER_UNSET);
+
+      /* we pretend the guard is for the parent */
+      pid = getppid ();
+      guard = bolt_power_acquire_full (power, "test", pid, &err);
+      g_assert_no_error (err);
+      g_assert_nonnull (guard);
+
+      state = bolt_power_get_state (power);
+      g_assert_cmpint (state, ==, BOLT_FORCE_POWER_ON);
+
+      exit (0);
+    }
+
+  /* parent */
+  pid = waitpid (pid, &r, 0);
+  g_assert_cmpint (pid, >, 0);
+  g_assert_cmpint (r, ==, 0);
+
+  /* now lets recover the guard */
+  power = make_bolt_power_timeout (tt, 10);
+
+  g_assert_no_error (err);
+  g_assert_nonnull (power);
+
+  state = bolt_power_get_state (power);
+  g_assert_cmpint (state, ==, BOLT_FORCE_POWER_ON);
+}
+
+static void
+test_power_recover_guards_fail (TestPower *tt, gconstpointer user)
+{
+  g_autoptr(BoltPower) power = NULL;
+  g_autoptr(BoltPowerGuard) guard = NULL;
+  g_autoptr(GError) err = NULL;
+  BoltPowerState state;
+  const char *fp;
+  pid_t pid;
+  int r;
+
+  fp = mock_sysfs_force_power_add (tt->sysfs);
+  g_assert_nonnull (fp);
+
+  pid = fork ();
+  g_assert_cmpint (pid, !=, -1);
+
+  if (pid == 0)
+    {
+      /* child */
+      power = make_bolt_power_timeout (tt, 10);
+
+      g_assert_no_error (err);
+      g_assert_nonnull (power);
+
+      state = bolt_power_get_state (power);
+      g_assert_cmpint (state, ==, BOLT_FORCE_POWER_UNSET);
+
+      /* we pass in zero as pid, which means it will be our pid */
+      guard = bolt_power_acquire_full (power, "test", 0, &err);
+      g_assert_no_error (err);
+      g_assert_nonnull (guard);
+
+      state = bolt_power_get_state (power);
+      g_assert_cmpint (state, ==, BOLT_FORCE_POWER_ON);
+
+      exit (0);
+    }
+
+  /* parent */
+  pid = waitpid (pid, &r, 0);
+  g_assert_cmpint (pid, >, 0);
+  g_assert_cmpint (r, ==, 0);
+
+  /* now lets recover the guard */
+  power = make_bolt_power_timeout (tt, 10);
+
+  g_assert_no_error (err);
+  g_assert_nonnull (power);
+
+  state = bolt_power_get_state (power);
+  g_assert_cmpint (state, ==, BOLT_FORCE_POWER_WAIT);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -463,6 +576,20 @@ main (int argc, char **argv)
               NULL,
               test_power_setup,
               test_power_recover_state,
+              test_power_tear_down);
+
+  g_test_add ("/power/guards/recover/ok",
+              TestPower,
+              NULL,
+              test_power_setup,
+              test_power_recover_guards_ok,
+              test_power_tear_down);
+
+  g_test_add ("/power/guards/recover/fail",
+              TestPower,
+              NULL,
+              test_power_setup,
+              test_power_recover_guards_fail,
               test_power_tear_down);
 
   return g_test_run ();
