e77de53a769f35ff1372b8efe19fba5655db6899
Christian Kellner <christian@kellner.me>
2018-12-20 18:34:23 +0100

    common: add implementation of sd_notify
    Add bolt_sd_notify_literal, a simple implementation of the IPC
    protocol used by systemd to communicate process status (and more).
    Rolling a custom implementation is easy enough and avoid linking
    to libsystemd.

--- common/bolt-unix.c
+++ common/bolt-unix.c
@@ -22,6 +22,16 @@
 
 #include "bolt-unix.h"
 
+#include "bolt-error.h"
+#include "bolt-io.h"
+
+#include <gio/gio.h>
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
 gboolean
 bolt_pid_is_alive (pid_t pid)
 {
@@ -34,4 +44,93 @@ bolt_pid_is_alive (pid_t pid)
     g_snprintf (path, sizeof (path), "/proc/self/stat");
 
   return g_file_test (path, G_FILE_TEST_EXISTS);
+}
+
+gboolean
+bolt_sd_notify_literal (const char *state,
+                        gboolean   *sent,
+                        GError    **error)
+{
+  bolt_autoclose int fd = -1;
+  struct sockaddr_un sau = {AF_UNIX, };
+  struct msghdr msghdr = { NULL, };
+  struct iovec iovec = { };
+  const char *env;
+  socklen_t socklen;
+  size_t len;
+  ssize_t r;
+
+  g_return_val_if_fail (state != NULL, FALSE);
+  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+
+  if (sent)
+    *sent = FALSE;
+
+  env = g_getenv ("NOTIFY_SOCKET");
+
+  if (env == NULL)
+    return TRUE;
+
+  len = strlen (env);
+
+  if (len > sizeof (sau.sun_path) - 1)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT,
+                   "unix domain socket to long: %s", env);
+      return FALSE;
+    }
+
+  socklen = offsetof (struct sockaddr_un, sun_path) + len;
+
+  switch (*env)
+    {
+    case '@':
+      /* abstract sockets have sun_path[0] set to '\0',
+       * otherwise '\0' have no special meaning, i.e.
+       * the address is not null-terminted */
+      memcpy (sau.sun_path + 1, env + 1, len);
+      socklen += 1; /* the leading '\0' */
+      break;
+
+    case '/':
+      /* pathname: null-terminated filesystem path */
+      memcpy (sau.sun_path, env, len + 1);
+      break;
+
+    default:
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+                   "unsupported socket address: %s", env);
+      return FALSE;
+    }
+
+  fd = socket (AF_UNIX, SOCK_DGRAM | SOCK_CLOEXEC, 0);
+  if (fd < 0)
+    return bolt_error_for_errno (error, errno, "failed to open socket %m");
+
+  iovec.iov_base = (void *) state;
+  iovec.iov_len = strlen (state);
+
+  msghdr.msg_iov = &iovec;
+  msghdr.msg_iovlen = 1;
+  msghdr.msg_name = &sau;
+  msghdr.msg_namelen = socklen;
+
+  r = sendmsg (fd, &msghdr, MSG_NOSIGNAL);
+
+  if (sent)
+    *sent = TRUE;
+
+  if (r < 0)
+    {
+      bolt_error_for_errno (error, errno, "failed to send msg: %m");
+      return FALSE;
+    }
+  else if ((size_t) r != iovec.iov_len)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_MESSAGE_TOO_LARGE,
+                   "failed to send complete message: %s", env);
+      return FALSE;
+    }
+
+  return TRUE;
 }
