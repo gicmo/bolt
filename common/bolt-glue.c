ce2eedc44113b841c2c2a6e686bd1fdfde7ab5f5
Christian Kellner <christian@kellner.me>
2019-02-19 14:06:45 +0100

    common: new helper to list and find gobject props
    Bundle them together in "bolt-glue", GObject library utilities and
    extensions, or something.
    Currently consists of two functions, one to list the property of
    a type (but exclude the properties of all sub-types). One to find
    a property with a name or nick in an array of properties.

---
+++ common/bolt-glue.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2019 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Christian J. Kellner <christian@kellner.me>
+ */
+
+#include "config.h"
+
+#include "bolt-glue.h"
+
+#include "bolt-str.h"
+
+
+GPtrArray *
+bolt_properties_for_type (GType target)
+{
+  GObjectClass *klass;
+  GParamSpec **specs;
+  GPtrArray *props;
+  guint n;
+
+  klass = g_type_class_ref (target);
+  specs = g_object_class_list_properties (klass, &n);
+  props = g_ptr_array_new_full (n, (GDestroyNotify) g_param_spec_unref);
+
+  for (guint i = 0; i < n; i++)
+    {
+      GParamSpec *s = specs[i];
+
+      if (s->owner_type != target)
+        continue;
+
+      g_ptr_array_add (props, g_param_spec_ref (s));
+    }
+
+  return props;
+}
+
+gboolean
+bolt_properties_find (GPtrArray   *specs,
+                      const char  *name,
+                      GParamSpec **spec)
+{
+
+  if (name == NULL || specs == NULL)
+    return FALSE;
+
+  for (guint i = 0; i < specs->len; i++)
+    {
+      GParamSpec *s = g_ptr_array_index (specs, i);
+
+      if (g_str_equal (name, s->name) ||
+          bolt_streq (name, g_param_spec_get_nick (s)))
+        {
+          *spec = s;
+          return TRUE;
+        }
+    }
+
+  return FALSE;
+}
---
+++ common/bolt-glue.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2019 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Christian J. Kellner <christian@kellner.me>
+ */
+
+#pragma once
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+GPtrArray *           bolt_properties_for_type (GType target);
+
+gboolean              bolt_properties_find (GPtrArray   *specs,
+                                            const char  *name,
+                                            GParamSpec **spec);
+
+G_END_DECLS
