8a03c55ceaeaa13c2ff42d831935544bb89abf9c
Christian Kellner <christian@kellner.me>
2019-06-11 19:53:15 +0200

    git-hook.sh: added in scripts/ to manage git hooks
    Can 'check', 'install' and 'uninstall' git hooks which are located
    in scripts/git-hooks/

---
+++ scripts/git-hooks.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# git hooks management
+
+# global setup
+set -u
+SRCDIR=${MESON_SOURCE_ROOT:-$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)}
+HOOKDIR="$SRCDIR/scripts/git-hooks/"
+DESTDIR="$SRCDIR/.git/hooks"
+
+cd "$SRCDIR"
+declare -a HOOKS=()
+readarray -t HOOKS < <(find "$HOOKDIR" -maxdepth 1 -type f -exec basename {} \;)
+
+# individual commands
+check () {
+    for hook in ${HOOKS[@]}; do
+        echo -n "$hook "
+        DEST="$DESTDIR/$hook"
+        if [ -x "$DEST" ]; then
+            echo "installed"
+        else
+            echo "missing"
+        fi
+    done
+}
+
+install () {
+    RESULT=0
+    for hook in ${HOOKS[@]}; do
+        DEST="$DESTDIR/$hook"
+        if [ -x "$DEST" ]; then
+            continue
+        fi
+
+        cp -p "$HOOKDIR/$hook" "$DEST"
+        chmod a+x "$DEST"
+        ((RESULT -= 1))
+        echo "Installed '$hook' hook"
+    done
+    exit $RESULT
+}
+
+uninstall () {
+    for hook in ${HOOKS[@]}; do
+        DEST="$DESTDIR/$hook"
+        if [ ! -x "$DEST" ]; then
+            continue
+        fi
+
+        rm "$DEST"
+        echo "Uninstalled '$hook' hook"
+    done
+}
+
+# main
+
+usage_error () {
+    echo "usage: $0 <check|install|uninstall>"
+    exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    usage_error
+fi
+
+case "$1" in
+    check)
+        check
+        ;;
+    install)
+        install
+        ;;
+    uninstall)
+        uninstall
+        ;;
+    *)
+        usage_error
+        ;;
+esac
