662e4931988be216060aa6f31297780daa578353
Christian Kellner <christian@kellner.me>
2019-09-03 13:06:11 +0200

    containers: provision the /build dir during build
    Instead of provisioning the /build dir in the build script, do it
    while building the container.

--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -5,8 +5,7 @@ set -x
 export LC_ALL=C.UTF-8
 export PYTHONPATH="/usr/share/glib-2.0"
 
-rm -rf /build
-mkdir /build
+rm -rf /build/*
 meson -Db_coverage=true -Dtests-speed=slow . /build
 ninja -C /build
 meson test -C /build --verbose
