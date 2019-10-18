a97daa6fa46f91c92a7edade6aa3b94eb350f8ea
Christian Kellner <christian@kellner.me>
2019-10-18 16:33:44 +0200

    scripts: add --replace option for uncrustify
    A new version of uncrustify generated files with the .uncrustify
    suffix when --replace was not specified.

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -3,5 +3,5 @@ SRCROOT=`git rev-parse --show-toplevel`
 CFG="$SRCROOT/scripts/uncrustify.cfg"
 echo "srcroot: $SRCROOT"
 pushd "$SRCROOT"
-uncrustify -c "$CFG" --no-backup `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v gvdb | grep -v build/`
+uncrustify -c "$CFG" --replace --no-backup `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v gvdb | grep -v build/`
 popd
