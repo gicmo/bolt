473dbe180b9f706546a6292ee080dac4221bd375
Christian Kellner <christian@kellner.me>
2019-10-31 18:21:28 +0100

    scripts/uncrustify: --check for check-only
    Introduce a --check that will not edit the files, but only check if
    the format is correct. Meant to be used in CI mostly.

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -2,6 +2,19 @@
 SRCROOT=`git rev-parse --show-toplevel`
 CFG="$SRCROOT/scripts/uncrustify.cfg"
 echo "srcroot: $SRCROOT"
+
+case "$1" in
+    -c|--check)
+	OPTS="--check"
+        ;;
+
+    *)
+	OPTS="--replace --no-backup"
+        ;;
+esac
+
 pushd "$SRCROOT"
-uncrustify -c "$CFG" --replace --no-backup `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v gvdb | grep -v build/`
+uncrustify -c "$CFG" $OPTS `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v gvdb | grep -v build/`
+RES=$?
 popd
+exit $RES
